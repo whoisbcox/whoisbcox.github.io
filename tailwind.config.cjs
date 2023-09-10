51a7965b9c1923bf6d5d9da2f206f8e885b3c0cc
Brandon <whoisbcox@gmail.com>
2023-09-10 02:52:51 -0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ["'Montserrat', sans-serif"]
+		},
+		colors: {
+			primary: '#B8C7CB',
+			secondary: '#E7EFF5',
+			background: '#001821',
+			highlight: '#0099ff',
+		},
 		extend: {},
 	},
 	plugins: [],
