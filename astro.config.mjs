9284f02a52d12dba58d26ddd67623ff11ee8822e
Brandon <whoisbcox@gmail.com>
2024-01-16 22:28:41 -0600

    Minor change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://whoisbcox.github.io',
   integrations: [tailwind(), react()],
+  site: 'https://whoisbcox.github.io',
 });
