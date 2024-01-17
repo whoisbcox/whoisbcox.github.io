6ac0d28766806f4105478291911741a110387a91
Brandon <whoisbcox@gmail.com>
2024-01-17 09:31:52 -0600

    Reverts site address

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://whoisbcox.com',
+  site: 'https://whoisbcox.github.io',
 });
